5f6514ba90c9f7160937df9c0cf9e9de4c4663fc
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-02 15:19:22 +0600

    api slice 🌊

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // import { RootState } from "../../app/store";
-
+// api slice
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
